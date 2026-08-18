1cfb18c73d041a2e40b5e79361035c1feeecc988
sukruthasrirangam <sukrutha.srirangam@gmail.com>
2026-08-18 02:46:27 -0400

    week 14: tests, trail detail view with 404, README screenshots

--- trails/tests.py
+++ trails/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Trail, Park
+from waypoint_core.distance import Distance
 
-# Create your tests here.
+
+class TrailQueryTests(TestCase):
+    def setUp(self):
+        self.park = Park.objects.create(name="Test Park", region="Test Region")
+        self.open_trail = Trail.objects.create(
+            name="Open Trail", distance_km=5.0, elevation_gain=200,
+            difficulty="easy", is_open=True, park=self.park
+        )
+        self.closed_trail = Trail.objects.create(
+            name="Closed Trail", distance_km=3.0, elevation_gain=100,
+            difficulty="moderate", is_open=False, park=self.park
+        )
+
+    def test_open_trails_query_excludes_closed(self):
+        open_trails = Trail.objects.filter(is_open=True)
+        self.assertIn(self.open_trail, open_trails)
+        self.assertNotIn(self.closed_trail, open_trails)
+
+    def test_trail_detail_404_for_missing_trail(self):
+        response = self.client.get(reverse('trail_detail', args=[9999]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_trail_detail_200_for_existing_trail(self):
+        response = self.client.get(reverse('trail_detail', args=[self.open_trail.id]))
+        self.assertEqual(response.status_code, 200)
+
+
+class DistanceRuleTests(TestCase):
+    def test_negative_distance_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            Distance(-5, "km")
--- trails/views.py
+++ trails/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Trail, Park
 
 
@@ -11,3 +11,8 @@ def trails_by_park(request, park_id):
     park = Park.objects.get(id=park_id)
     trails = Trail.objects.filter(park=park, is_open=True).order_by('distance_km')
     return render(request, "catalog.html", {"trails": trails})
+
+
+def trail_detail(request, trail_id):
+    trail = get_object_or_404(Trail, id=trail_id)
+    return render(request, "trail_detail.html", {"trail": trail})
